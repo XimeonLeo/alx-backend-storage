b8fd629817ba2015eb3d1bec432c12e3767d072c
XimeonLeo <ximeonleo@gmail.com>
2024-01-23 02:11:25 +0000

    git all

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a table with name `users` and added more param
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
