ddcd025dfcfd6cdb8ae8fec9b04c625b87b1616d
XimeonLeo <ximeonleo@gmil.com>
2024-03-01 21:26:33 +0000

    completed all task for redis basic

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Module for Redis db """
+import redis
+from uuid import uuid4
+from typing import Union, Callable, Optional
+
+
+UnionOfTypes = Union[str, bytes, int, float]
+
+
+class Cache:
+    """ Class for methods for operating a caching system
+    """
+
+    def __init__(self):
+        """ Instance of the Redis database
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: UnionOfTypes) -> str:
+        """ Takes a data argument and returns a string
+        """
+        self._key = str(uuid4())
+        self._redis.set(self._key, data)
+        return self._key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> UnionOfTypes:
+        """
+        Retrieves data stored in redis using a key and then
+        converts the result/value back to the desired format
+        """
+        value = self._redis.get(key)
+        return fn(value) if fn else value
+
+    def get_str(self, value: str) -> str:
+        """ A method that returns a string
+        """
+        return self.get(self._key, str)
+
+    def get_int(self, value: str) -> int:
+        """ A method that returns an integer
+        """
+        return self.get(self._key, int)
